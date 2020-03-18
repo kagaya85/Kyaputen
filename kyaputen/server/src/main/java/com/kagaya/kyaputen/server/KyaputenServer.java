251ba13bdb1048c13a4a04bd933b5e7d4094c577
Kagaya <kagaya85@outlook.com>
2020-03-18 22:55:16 +0800

    add: grpc demo code

---
+++ kyaputen/server/src/main/java/com/kagaya/kyaputen/server/KyaputenServer.java
@@ -0,0 +1,57 @@
+package com.kagaya.kyaputen.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import com.kagaya.kyaputen.grpc.TaskServiceGrpc;
+import com.kagaya.kyaputen.grpc.TaskServicePb.PollRequest;
+import com.kagaya.kyaputen.grpc.TaskServicePb.PollResponse;
+import com.kagaya.kyaputen.proto.TaskPb.Task;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+
+public class KyaputenServer {
+
+    private int port;
+    private Server server;
+
+    public KyaputenServer(int bindPort) {
+        port = bindPort;
+    }
+
+    public void start() throws IOException {
+        server = ServerBuilder.forPort(port)
+                .addService(new TaskServiceImpl())
+                .build()
+                .start();
+    }
+
+    public void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    private class TaskServiceImpl extends TaskServiceGrpc.TaskServiceImplBase {
+
+        @Override
+        public void poll(PollRequest request, StreamObserver<PollResponse> responseObserver) {
+            System.out.println(("Request Task Type: ") + request.getTaskType());
+            System.out.println("Request Domain: " + request.getDomain());
+            System.out.println(("Request Worker Id: ") + request.getWorkerId());
+
+            Task task = Task.newBuilder().setStartTime(System.currentTimeMillis()/1000L).build();
+            PollResponse response = PollResponse.newBuilder().setTask(task).build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+    }
+
+}
