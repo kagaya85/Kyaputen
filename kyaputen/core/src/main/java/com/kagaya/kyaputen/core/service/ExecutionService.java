814393b8725c3df17f57d0d2873ca79085380941
kagaya <kagaya85@outlook.com>
2020-03-25 16:45:49 +0800

    add: Executon service poll funciton

--- kyaputen/core/src/main/java/com/kagaya/kyaputen/core/service/ExecutionService.java
+++ kyaputen/core/src/main/java/com/kagaya/kyaputen/core/service/ExecutionService.java
@@ -1,10 +1,17 @@
 package com.kagaya.kyaputen.core.service;
 
 import com.kagaya.kyaputen.core.dao.QueueDAO;
+import com.kagaya.kyaputen.common.metadata.tasks.Task;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
+/**
+ * @description 逻辑部分代码
+ */
+
+@Singleton
 public class ExecutionService {
 
     private static final Logger logger = LoggerFactory.getLogger(ExecutionService.class);
@@ -20,5 +27,24 @@ public class ExecutionService {
     @Inject
     public ExecutionService(QueueDAO queueDAO) {
         this.queueDAO = queueDAO;
+    }
+
+    public Task poll(String taskType, String workId) {
+        return poll(taskType, workId, null);
+    }
+
+    public Task poll(String taskType, String workerId, String domain) {
+        List<Task> tasks = poll(taskType, workerId, domain, i, 100);
+        
+        if (tasks.isEmpty()) {
+            return null;
+        
+        }
+        else
+            reutrn tasks.get(0);
+    }
+
+    public Task poll(String taskType, String workerId, String domain, int count, int timeoutMilliSecond) {
+
     }
 }
