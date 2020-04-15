8acf659608c8297606b81e9e5e69c39d7399be94
Kagaya <kagaya85@outlook.com>
2020-04-16 01:19:05 +0800

    add: DecideService decide func

--- kyaputen/core/src/main/java/com/kagaya/kyaputen/core/service/DecideService.java
+++ kyaputen/core/src/main/java/com/kagaya/kyaputen/core/service/DecideService.java
@@ -1,26 +1,42 @@
 package com.kagaya.kyaputen.core.service;
 
 import com.kagaya.kyaputen.common.metadata.tasks.Task;
+import com.kagaya.kyaputen.common.metadata.tasks.Task.Status;
 import com.kagaya.kyaputen.common.runtime.Workflow;
+import com.kagaya.kyaputen.core.dao.ExecutionDAO;
 
+import javax.inject.Inject;
 import java.util.LinkedList;
 import java.util.List;
 
 public class DecideService {
 
+    private ExecutionDAO executionDAO;
+
+    @Inject
+    public DecideService(ExecutionDAO executionDAO) {
+        this.executionDAO = executionDAO;
+    }
     /**
      *
      * @param workflowId
      * @description 
      * @return DecideOutcome
      */
-    public DecideOutcome decide(final Workflow workflowId) {
+    public DecideOutcome decide(String workflowId) {
+        Workflow workflow = executionDAO.getWorkflow(workflowId);
 
+        DecideOutcome outcome = new DecideOutcome();
 
+        List<Task> taskList = workflow.getTasks();
 
+        for (Task task: taskList) {
+            if (task.getStatus().equals(Status.SCHEDULED)) {
+                outcome.tasksToBeScheduled.add(task);
+            }
+        }
 
-
-        return new DecideOutcome();
+        return outcome;
     }
 
     public static class DecideOutcome {
